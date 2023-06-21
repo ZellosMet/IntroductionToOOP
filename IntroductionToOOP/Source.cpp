8a6383ceb8df6cb83c4d0bfb2850dd183350dade
Zello <Zello@DESKTOP-IBMII35>
2023-06-21 23:38:51 +0900

    Constructors

--- IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/Source.cpp
@@ -2,26 +2,36 @@
 #include<ctime>
 using namespace std;
 
-class Point //создание структуры
+class Point //создание класса
 {
 	double x;
 	double y;
 
 public: // реализация сетторов и геттеров
+	/*
 	Point() //Конструктор без параметров
 	{
-		x = rand() % 10;
-		y = rand() % 10;		
+		x = y = double(); //type() - то значение по умолчанию для заданного типа. таким образом мы якобы вызываем конструктор по умолчанию для заданного типа
+		cout << "Конструктор по умолчанию\t\t" << this << endl;
 	}
-	Point(double x, double y) //Конструктор с параметрами
+	Point(double x)
+	{	
+		this->x = x;
+		this->y = 0;
+		cout << "Конструктор с одним параметром:\t\t" << this << endl;
+	}
+	*/
+	Point(double x = 0, double y = 0) //Универсальный Конструктор с параметрами
 	{
 		this->x = x;
 		this->y = y;
+		cout << "Конструктор с двумя параметрами:\t" << this << endl;
 	}
-	Point(const Point &cp) // Конструктор копирования
+	Point(const Point &other) // Конструктор копирования
 	{
-		x = cp.x;
-		y = cp.y;
+		x = other.x;
+		y = other.y;
+		cout << "Конструктор копирования:\t\t" << this << endl;
 	}
 	double get_x()const
 	{
@@ -43,7 +53,7 @@ public: // реализация сетторов и геттеров
 	{
 		return sqrt(pow(x, 2) + pow(y, 2));
 	}
-	double distancePointToPoint(Point b)const
+	double distancePointToPoint(const Point &b)const
 	{
 		return sqrt(pow(b.x - x, 2) + pow(b.y - y, 2));
 	}
@@ -53,14 +63,25 @@ public: // реализация сетторов и геттеров
 	}
 	~Point() // Деконструктор
 	{
-		cout << "Точка уничтожина (" << x << ", " << y << " )" << endl;
+		cout << "Деконструктор:\t\t\t\t" << this << endl;
+	}
+	// оператор:
+	Point &operator=(const Point& other) //возвращаем по ссылки
+	{	
+		this-> x = other.x;
+		this-> y = other.y;
+		cout << "Оператор копирования:\t\t\t" << this << endl;
+		return *this; // возвращаем разъименованное значение
 	}
 };
 
-double Distance(Point a, Point b);
+double Distance(Point &a, Point &b);
 
 //#define STRUCT_POINT
-#define HOME_WORK
+//#define DISTANCE_CHECK
+//#define CONSTUCTORS_CHECK
+
+#define delim "\n-----------------------------------------------\n"
 
 void main()
 {
@@ -75,22 +96,40 @@ void main()
 	Point *pA = &A;
 	cout << pA->x << "\t" << pA->y << endl; //Обращение к полю по адресу
 #endif 
-
-#ifdef HOME_WORK
+#ifdef DISTANCE_CHECK
 	Point A(2, 3); // Создание объекта конструктором с параметрами
 	Point B; //Создание объекта конструктором без параметров
-	Point A_cp{ A }; // Вызов конструктора копирования
 	cout << "Сгенерированные координаты точки 'А': (" << A.get_x() << ", " << A.get_y() << ")" << endl;
-	cout << "Координаты копии точки 'А' : (" << A_cp.get_x() << ", " << A_cp.get_y() << ")" << endl;
 	cout << "Сгенерированные координаты точки 'B': (" << B.get_x() << ", " << B.get_y() << ")" << endl;
 	cout << "Расстояние до точки 'А': " << A.distance() << endl;
 	cout << "Расстояние до точки 'В': " << B.distance() << endl;
+	cout << delim << endl;
 	cout << "Расчёт расстояние между точками через функцию: " << Distance(A, B) << endl;
+	cout << delim << endl;
 	cout << "Расстояние между точками методом класса: " << A.distancePointToPoint(B) << endl;
 #endif
+#ifdef CONSTUCTORS_CHECK
+	//Методы создания объектов перегрузкой конструктора
+	Point A;
+	A.print();
+	Point B = 5;
+	B.print();
+	Point C(2, 3);
+	C.print();
+	Point D = C; //конструктор копирования
+	D.print();
+	Point E;
+	E = D; //оператор присваивания
+	E.print();
+#endif 
+	Point A, B, C;
+	A.print();
+	B.print();
+	C.print();
+	A = B = C = Point(2, 3);
 }
 
-double Distance(Point a, Point b)
+double Distance(Point &a, Point &b)
 {
 	return sqrt(pow((b.get_x() - a.get_x()), 2) + pow((b.get_y() - a.get_y()), 2));
 }
