713d8d98f0c821812e6e4b3a3faf4e3fb2db4fdb
Zello <Zello@DESKTOP-IBMII35>
2023-07-05 20:26:58 +0900

    Strings V0.1

--- String/Source.cpp
+++ String/Source.cpp
@@ -6,29 +6,37 @@ class Strings
 	int size = 0;
 public:
 //Конструкторы
-	Strings()
+	Strings() //Конструктор поумолчанию
 	{
 		char* str = new char[80]{};
 		this->str = str;
 	}
-	Strings(int size)
+	Strings(int size) //Конструктор с параметром размера строки
 	{
+		this->size = size;
 		char* str = new char[size] {};
 		this->str = str;
 	}
-	Strings(const char *str)
+	Strings(const char *str) //Конструктор для с пораметром строки
 	{
 		this->size = strlen(str);
 		this->str = new char[size+1] {};
 		for (int i = 0; i < size; i++) this->str[i] = str[i];
 		this->str[size] = '\0';
 	}
-	Strings(const Strings& str)
+	Strings(const Strings& str) //Конструктор копирования
 	{
 		this-> size = strlen(str.str);
 		this->str = new char[size+1] {};
 		for (int i = 0; i < size; i++) this->str[i] = str.str[i];
 		this->str[size] = '\0';
+	}
+	Strings(Strings&& str)noexcept //Конструктор переноса
+	{
+		this->str = str.str;
+		this->size = str.size;
+		str.str = nullptr;
+		str.size = 0;
 	}
 //Геттры
 	int get_size()const
@@ -67,7 +75,7 @@ std::ostream& operator<<(std::ostream& os, const Strings& str)
 }
 Strings operator+(const Strings& left, const Strings& right)
 {
-	Strings concate = left.get_size() + right.get_size() + 1;
+	Strings concate = left.get_size() + right.get_size() + 2;
 	int i = 0;
 	for (; i < left.get_size(); i++) concate[i] = left[i];
 	for (int j = 0; j < right.get_size(); j++, i++) concate[i] = right[j];
@@ -77,14 +85,13 @@ Strings operator+(const Strings& left, const Strings& right)
 
 void main()
 {
-
 	setlocale(LC_ALL, "ru");
 	Strings str1;
 	Strings str2 = 5;
 	Strings str3 = "Hello";
-	std::cout << str3 << std::endl;
+	std::cout << "Первая строка: " << str3 << std::endl;
 	Strings str4 = "World";
-	std::cout << str4 << std::endl;
+	std::cout << "Первая строка: " << str4 << std::endl;
 	Strings str5 = str3 + str4;
-	std::cout << str5 << std::endl;
+	std::cout << "Конкатенация строк: " << str5 << std::endl;
 }
